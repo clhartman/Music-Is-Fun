bf89b0a2ebf88fda77c9cfadb669542dc4a1cb04
CourtneyHartman <courtneylarine@gmail.com>
2019-05-19 20:17:37 -0600

    Styling Changes

--- app/models/Song.js
+++ app/models/Song.js
@@ -16,10 +16,10 @@ export default class Song {
                 <img class="card-img-top" src=${this.albumArt} alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">${this.title}</h5>
-                    <p class="card-text"><b>Artist Name:</b> ${this.artist}</p>
-                    <audio controls style="width: 200px;"src="${this.preview}">${this.title}</audio>
-                    <p class="card-text">Album Title: ${this.collection}</p>
-                    <p class="card-text">Album Price: ${this.price}</p>
+                    <p class="card-text"><b>Artist:</b> ${this.artist}</p>
+                    <audio controls style="width: 150px; height: 30px;"src="${this.preview}">${this.title}</audio>
+                    <p class="card-text"><b>Album: </b>${this.collection}</p>
+                    <p class="card-text"><b>Price: </b>$${this.price}</p>
                 </div>
             </div>
         </div>
